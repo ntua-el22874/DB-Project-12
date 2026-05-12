1a57b031b996c875b3d445b1d543b0340b5ee3f5
ntua-el22874 <el22874@mail.ntua.gr>
2026-05-12 03:37:41 +0300

    added query 1

---
+++ Queries/Q01.sql
@@ -0,0 +1,12 @@
+SELECT
+    SUM(total_cost) AS total_cost,
+    SUM(total_cost - base_cost) AS total_additional_cost,
+    SUM(base_cost) AS total_base_cost,
+    dept_name,
+    YEAR(discharge_date) AS year,
+    insurance,
+    Hospitalization.ken_code
+FROM Hospitalization JOIN KEN ON Hospitalization.ken_code = KEN.ken_code
+    JOIN Patient ON Patient.amka = Hospitalization.patient_id
+GROUP BY dept_name, YEAR(discharge_date), insurance, Hospitalization.ken_code
+ORDER BY dept_name, year;
