ac0751d06a9669f919778d109871dc63bf016907
Aris68 <arvagelatos@gmail.com>
2026-05-12 18:46:00 +0300

    load staff_department

--- load_list.sql
+++ load_list.sql
@@ -1,93 +1,99 @@
 -- cannot be run from sqltools(vscode) must be run from terminal
-LOAD DATA LOCAL INFILE '/home/aris/DB/DB-Project-12/ListForDB_csv/Staff_Clean.csv'
+LOAD DATA LOCAL INFILE './ListForDB_csv/Staff_Clean.csv'
 INTO TABLE Staff 
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n' 
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/aris/DB/DB-Project-12/ListForDB_csv/Nurse_Clean.csv' 
+LOAD DATA LOCAL INFILE './ListForDB_csv/Nurse_Clean.csv' 
 INTO TABLE Nurse 
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n' 
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/aris/DB/DB-Project-12/ListForDB_csv/AdminStaff_Clean.csv'
+LOAD DATA LOCAL INFILE './ListForDB_csv/AdminStaff_Clean.csv'
 INTO TABLE AdminStaff
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/aris/DB/DB-Project-12/ListForDB_csv/Department_Clean.csv'
+LOAD DATA LOCAL INFILE './ListForDB_csv/Department_Clean.csv'
 INTO TABLE Department
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/aris/DB/DB-Project-12/ListForDB_csv/Doctor_Clean.csv'
+LOAD DATA LOCAL INFILE './ListForDB_csv/Doctor_Clean.csv'
 INTO TABLE Doctor
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/aris/DB/DB-Project-12/ListForDB_csv/Bed_Clean.csv'
+LOAD DATA LOCAL INFILE './ListForDB_csv/Staff_Department_Clean.csv'
+INTO TABLE Staff_Department
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE './ListForDB_csv/Bed_Clean.csv'
 INTO TABLE Bed
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/aris/DB/DB-Project-12/ListForDB_csv/Patients_Clean.csv'
+LOAD DATA LOCAL INFILE './ListForDB_csv/Patients_Clean.csv'
 INTO TABLE Patient
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/aris/DB/DB-Project-12/ListForDB_csv/ICD10_codes.csv'
+LOAD DATA LOCAL INFILE './ListForDB_csv/ICD10_codes.csv'
 INTO TABLE ICD10
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/aris/DB/DB-Project-12/ListForDB_csv/KEN_codes.csv'
+LOAD DATA LOCAL INFILE './ListForDB_csv/KEN_codes.csv'
 INTO TABLE KEN
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/aris/DB/DB-Project-12/ListForDB_csv/Hospitalizations_Clean.csv'
+LOAD DATA LOCAL INFILE './ListForDB_csv/Hospitalizations_Clean.csv'
 INTO TABLE Hospitalization
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/aris/DB/DB-Project-12/ListForDB_csv/Hospitalization_Ratings_Clean.csv'
+LOAD DATA LOCAL INFILE './ListForDB_csv/Hospitalization_Ratings_Clean.csv'
 INTO TABLE HospitalizationRating
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 -- up to here everything loads correctly
 
-LOAD DATA LOCAL INFILE '/home/aris/DB/DB-Project-12/ListForDB_csv/Medical_Acts_Clean.csv'
+LOAD DATA LOCAL INFILE './ListForDB_csv/Medical_Acts_Clean.csv'
 INTO TABLE MedicalProcedure
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/aris/DB/DB-Project-12/ListForDB_csv/Shifts_Clean.csv'
+LOAD DATA LOCAL INFILE './ListForDB_csv/Shifts_Clean.csv'
 INTO TABLE Shift
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/aris/DB/DB-Project-12/ListForDB_csv/Substances_Clean.csv'
+LOAD DATA LOCAL INFILE './ListForDB_csv/Substances_Clean.csv'
 INTO TABLE Substance
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/aris/DB/DB-Project-12/ListForDB_csv/Triage_Clean.csv'
+LOAD DATA LOCAL INFILE './ListForDB_csv/Triage_Clean.csv'
 INTO TABLE Triage
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
