544f5d1ddf83d8d05ae5cbbc402ff6b5c294398f
Lazareos9999 <Lazareos@gmail.com>
2026-05-18 00:54:50 +0300

    Update load_1.sql

--- sql/load_1.sql
+++ sql/load_1.sql
@@ -4,4 +4,4 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
-(patient_id, bed_id, dept_name, admission_date, discharge_date, diagnosis_in, diagnosis_out, ken_code, total_cost);
+(hosp_id, patient_id, bed_id, dept_name, admission_date, discharge_date, diagnosis_in, diagnosis_out, ken_code, total_cost);
