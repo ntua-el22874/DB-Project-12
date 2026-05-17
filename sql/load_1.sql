fa914c4e76bfbb8399dda77690245aa4bc7de7da
Lazareos9999 <Lazareos@gmail.com>
2026-05-18 00:26:49 +0300

    Create load_1.sql

---
+++ sql/load_1.sql
@@ -0,0 +1,7 @@
+LOAD DATA LOCAL INFILE './ListForDB_csv/Hospitalization.csv'
+INTO TABLE Hospitalization
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(patient_id, bed_id, dept_name, admission_date, discharge_date, diagnosis_in, diagnosis_out, ken_code, total_cost);
