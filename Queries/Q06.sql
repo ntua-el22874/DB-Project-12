6eee98245dfb0f77f2f2da6669b01da9288151db
ntua-el22874 <el22874@mail.ntua.gr>
2026-05-19 17:46:09 +0300

    minor changes and added query outputs

--- Queries/Q06.sql
+++ Queries/Q06.sql
@@ -3,10 +3,11 @@ SELECT
     Hospitalization.hosp_id,
     Hospitalization.admission_date,
     Hospitalization.total_cost,
-    icd_in.description,
-    icd_out.description,
-    (medical_care + nursing_care + cleanliness + food + overall)/5.0 AS average_rating
-FROM Hospitalization LEFT JOIN HospitalizationRating ON Hospitalization.hosp_id = HospitalizationRating.hosp_id
+    icd_in.description AS diagnosis_in_desc,
+    icd_out.description AS diagnosis_out_desc,
+    (IFNULL(medical_care,0) + IFNULL(nursing_care,0) + IFNULL(cleanliness,0) + IFNULL(food,0) + IFNULL(overall,0))/5.0 AS average_rating
+FROM Hospitalization
+    LEFT JOIN HospitalizationRating ON Hospitalization.hosp_id = HospitalizationRating.hosp_id
     LEFT JOIN ICD10 AS icd_in ON Hospitalization.diagnosis_in = icd_in.code
     LEFT JOIN ICD10 AS icd_out ON Hospitalization.diagnosis_out = icd_out.code
-WHERE Hospitalization.patient_id = 79193941824 ;
+WHERE Hospitalization.patient_id = 79193941824;
